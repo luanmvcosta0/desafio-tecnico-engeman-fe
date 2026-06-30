d03c89dbd25439c6aa61736b1ba6636527910efa
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-30 15:54:06 -0300

    feat: implement login functionality with error handling in LoginForm component

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -15,12 +15,31 @@ import {
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/contexts/AuthProvider";
+import React, { useState } from "react";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const { login } = useAuth()!;
   const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  async function handleSubmite(e: React.FormEvent) {
+    e.preventDefault();
+    setError("");
+
+    try {
+      await login(email, password);
+      navigate("/");
+    } catch (err: any) {
+      setError(err.response?.data?.message || "Email ou senha inválidos");
+    }
+  }
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
@@ -34,7 +53,7 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmite}>
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="email">Email</FieldLabel>
@@ -42,6 +61,8 @@ export function LoginForm({
                   id="email"
                   type="email"
                   placeholder="seuemail@email.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                 />
               </Field>
@@ -53,9 +74,12 @@ export function LoginForm({
                   id="password"
                   type="password"
                   placeholder="********"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                 />
               </Field>
+              {error && <p className="text-sm text-red-500">{error}</p>}
               <Field>
                 <Button type="submit">Login</Button>
                 <FieldDescription className="text-center">

49fd5e190a98378de3b2d2ad50ce3a368b7bdc12
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-30 14:21:23 -0300

    feat: update AuthContextType interface to use email and password for login

--- src/interfaces/AuthContextType.ts
+++ src/interfaces/AuthContextType.ts
@@ -2,6 +2,12 @@ import type { User } from "./User";
 
 export interface AuthContextType {
   user: User | null;
-  login: (userData: User) => void;
+  login: (email: string, password: string) => Promise<void>;
+  register: (
+    username: string,
+    email: string,
+    password: string,
+    role?: string,
+  ) => Promise<void>;
   logout: () => void;
 }
